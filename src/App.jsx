c35d167125f7ed68a50b530e5a5429ed536081ba
David Vaughan <dvaug86@gmail.com>
2023-10-22 20:22:01 -0400

    started basic framework

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TaskForm from "./TaskForm";
+
+function App() {
+  return (
+    <div>
+      <TaskForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TaskForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TaskForm = () => {
+  return (
+    <form>
+    <button>+</button>
+    <input type="text" placeholder='Your next task...' />
+  </form>
+  )
+}
+
+export default TaskForm
